f5372c5003a8d4e1b9f37a28bc00c22533ec76f3
Ayodhya <IT24102395@my.sliit.lk>
2025-08-16 16:48:52 +0530

    Initial commit

---
+++ CeyMedInsurance/src/main/java/com/healthinsurancemanagementsystem/CeyMedInsurance/CeyMedInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.healthinsurancemanagementsystem.CeyMedInsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeyMedInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeyMedInsuranceApplication.class, args);
+	}
+
+}
